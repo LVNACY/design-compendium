29a881afc089dcc0f590ffd8b2deab1e09d08b35
effem <5085876+ephemeralrogue@users.noreply.github.com>
2024-11-01 14:59:22 -0700

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/your-repo-name',
+	output: 'export',
+	basePath: '/design-compendium',
 };
 
 export default nextConfig;
